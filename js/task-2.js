9049f73429d5cea7af1916dfb8b4befdd2e0234a
Myroslav Raks <raksmyroslav@gmail.com>
2024-04-28 11:03:30 +0200

    uncomment

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
-/* function formatMessage(message, maxLength) {
+function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message;
     } else {
@@ -12,4 +12,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
- */
